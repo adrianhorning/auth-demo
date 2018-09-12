e730cf946310125d58c0c7ca740ca3620c5bf864
Adrian Horning <adrian.horning@macc02tt1vahtd7.corp.zefr.com>
2018-09-12 12:13:06 -0600

    TypeScript implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
-import Hello from './components/Hello';
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
+// import logo from './logo.svg';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          {/* <img src={logo} className="App-logo" alt="logo" /> */}
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <Hello compiler="TypeScript" framework="React" />,
       </div>
     );
   }
